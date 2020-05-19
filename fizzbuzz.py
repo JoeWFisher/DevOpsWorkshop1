13ccb2f869a02b925144e24611e75a54973cee65
jfisherw <j.fisher.w@gmail.com>
2020-05-19 19:48:09 +0100

    Improvment to standard fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,13 +10,16 @@ def bang(number):
     if number % 7 == 0:
         return True
 
-for i in range(1, 101):
-    if fizz(i) and buzz(i) == True:
-        print('FizzBuzz')
-    elif fizz(i) == True:
-        print('Fizz')
-    elif buzz(i) == True:
-        print('Buzz')
+i = [1,'']
+
+for i[0] in range(1, 101):
+    if fizz(i[0]) and buzz(i[0]) == True:
+        i[1] = 'FizzBuzz'
+    elif fizz(i[0]) == True:
+        i[1] = 'Fizz'
+    elif buzz(i[0]) == True:
+        i[1] = 'Buzz'
     else:
-       print(i)
-    i + 1
+       i[1] = str(i[0])
+    print(i[1])
+    i[0] + 1

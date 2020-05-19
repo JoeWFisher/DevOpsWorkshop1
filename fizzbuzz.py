8b4a36f08ea8b30dd3efbe48807a6fb671672585
jfisherw <j.fisher.w@gmail.com>
2020-05-19 22:12:39 +0100

    Adding 'Bang' to FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,17 +9,24 @@ def buzz(number):
 def bang(number):
     if number % 7 == 0:
         return True
+    
 
 i = [1,'']
 
 for i[0] in range(1, 101):
+    i[1] = str(i[0])
     if fizz(i[0]) and buzz(i[0]) == True:
         i[1] = 'FizzBuzz'
     elif fizz(i[0]) == True:
         i[1] = 'Fizz'
     elif buzz(i[0]) == True:
         i[1] = 'Buzz'
-    else:
-       i[1] = str(i[0])
+
+    if bang(i[0]) == True:
+        if i[1] in ['FizzBuzz', 'Fizz', 'Buzz']:
+            i[1] = i[1] + 'Bang'
+        else:
+            i[1] = 'Bang'
+
     print(i[1])
     i[0] + 1

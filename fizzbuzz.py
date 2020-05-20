93e53df61d4a588999158e9f3fa684592f01adfc
JoeWFisher <joseph.fisher@softwire.com>
2020-05-20 09:28:51 +0100

    Adding 'Fuzz' to FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,12 +13,17 @@ def bang(number):
 def bong(number):
     if number % 11 == 0:
         return True
-    
+
+def fuzz(number):
+    if number % 13 == 0:
+        return True    
 
 i = [1,'']
 
-for i[0] in range(1, 101):
+for i[0] in range(1, 201):
+
     i[1] = str(i[0])
+    
     if bong(i[0]):
         i[1] = 'Bong'
     elif fizz(i[0]) and buzz(i[0]):
@@ -27,13 +32,20 @@ for i[0] in range(1, 101):
         i[1] = 'Fizz'
     elif buzz(i[0]):
         i[1] = 'Buzz'
-    
 
     if bang(i[0]):
         if i[1] in ['FizzBuzz', 'Fizz', 'Buzz']:
             i[1] = i[1] + 'Bang'
+        elif i[1] == 'Bong':
+            i[1] = 'Bong'
         else:
             i[1] = 'Bang'
+
+    if fuzz(i[0]):
+        if i[1] in ['FizzBuzz', 'Fizz', 'Buzz', 'Bong']:
+            i[1] = 'Fuzz' + i[1]
+        else:
+            i[1] = 'Fuzz'
 
     print(i[1])
     i[0] + 1
